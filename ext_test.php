470057f5d9c571f61d527d23286b613fe640f9f3
Timothy Gruber <tjgruber@outlook.com>
2018-07-22 22:19:21 -0700

    Integrated html forms and future php form processor

--- ext_test.php
+++ ext_test.php
@@ -1,15 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<title>EXT TEST</title>
+<!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+</head>
+
+<body>
+
 <?php
+
+//Set the minumim and maxium extensions to display
+$f_ext = 100;
+$l_ext = 199;
+
+//including the future php file that processes the form
+include "ext_test_form_processor.php";
 
 //File to use... may work with $wrets, I can't test that.
 $txt_file = 'ext_test.txt';
 
 //Check if file exists, if not
 if (file_exists($txt_file)) {
-    echo "The file $txt_file exists and is readable.";
     echo "<br><br><br>";
+//    echo "<center>The file <strong>$txt_file</strong> exists and is readable.</center>";
+    echo "<br>";
 }
 else {
-    echo "The file $txt_file does not exist, and is not readable.";
+    echo "The file $txt_file does not exist.";
 }
 
 //grabs contents of the text file.
@@ -31,10 +50,75 @@ if ($handle) {
     echo "Can't open file.";
 }
 
-//removes the "/" character in front of each extension.
-$extensions = str_replace("/", "", implode("<br>", $matches[0]));
+?>
 
-//echos results
-echo $extensions;
+<!-- Start of form, uses built-in htmlspecialchars security function as added precaution:
+        http://php.net/manual/en/function.htmlspecialchars.php -->
+<form enctype="multipart/form-data" method="post" action=<?php print htmlspecialchars($_SERVER["PHP_SELF"]); ?>>
+
+<?php 
+if (!($_SERVER["REQUEST_METHOD"] == "POST")) {
+    
+?>
+
+<center>
+<fieldset style="width:270px"><legend class="legendtitle">Select extension(s) to reboot:</legend>
+
+<!-- begin table -->
+<center>
+<table border="1" cellspacing="3" cellpadding="3">
+    <thead>
+        <tr>
+            <th><center><h4>Reboot?</h4></center></th>
+            <th><center><h4>Extension</h4></center></th>
+        </tr>
+    </thead>
+    <tbody>
+<?php } ?>
+
+<?php
+
+// print default table using file
+if (!($_SERVER["REQUEST_METHOD"] == "POST")) {
+    foreach($matches[1] as $item => $value) {
+        if($value >= $f_ext && $value <= $l_ext) {
+            echo "\t\t<tr>\n\t\t\t<td align=\"center\"><input type=\"checkbox\" name=\"extension\" value=\"$value\"></td>\n";
+            echo "\t\t\t<td align=\"center\"><strong>$value</strong></td>\n";
+        }
+    }
+
+    print "</tbody>\n";
+    print "</table>\n"; // end table
+    print "<br>\n";
+    print "<p align=\"center\"><input type=\"submit\" name=\"submit\" value=\"Reboot\"></p>";
+    echo "</fieldset>";
+
+} else {
+    ?>
+    <center>
+    <table border="1" cellspacing="3" cellpadding="3">
+    <thead>
+        <tr>
+            <th><center><h4>Submision Results:</h4></th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php 
+
+// Submission results
+    print "\t\t<tr>\n\t\t\t<td>Placeholder for later...</td>\n\t\t</tr>\n";
+    print "</tbody>\n";
+    echo "</table>";
+
+}
 
 ?>
+
+</center>
+
+</form>
+
+</body>
+
+</html>
