4c6e8916fdedeba38ed1f16bfbd6669b64b5e540
Jared Busch <jbusch@dt-jared.jaredbusch.com>
2018-07-23 09:50:26 -0500

    added php form from pull #2

--- yealink.reload.php
+++ yealink.reload.php
@@ -1,3 +1,12 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<title>Reload Yealink Phones</title>
+<!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+</head>
+
 <?php
 require_once 'AstMan.php';
 
@@ -27,14 +36,77 @@ $astman=new AstMan;
     //strip out all the /### values
     preg_match_all($pattern,$endpoints,$matches);
 
-    // loop to look at only the extensions defined above
+?>
+
+<body>
+    <!-- Start of form, uses built-in htmlspecialchars security function as added precaution:
+    http://php.net/manual/en/function.htmlspecialchars.php -->
+    <form enctype="multipart/form-data" method="post" action=<?php print htmlspecialchars($_SERVER["PHP_SELF"]); ?>>
+
+<?php 
+if (!($_SERVER["REQUEST_METHOD"] == "POST")) {
+    
+?>
+
+    <center>
+    <fieldset style="width:270px"><legend class="legendtitle">Select extension(s) to reboot or reload:</legend>
+
+    <!-- begin table -->
+    <center>
+    <table border="1" cellspacing="3" cellpadding="3">
+        <thead>
+            <tr>
+                <th><center><h4>Selected</h4></center></th>
+                <th><center><h4>Extension</h4></center></th>
+            </tr>
+        </thead>
+        <tbody>
+<?php } ?>
+
+<?php
+
+// print default table using file
+if (!($_SERVER["REQUEST_METHOD"] == "POST")) {
     foreach($matches[1] as $item => $value) {
         if($value >= $f_ext && $value <= $l_ext) {
-            echo "item: ";
-            echo $item;
-            echo "   value: ";
-            echo $value;
-            echo "<br />\r\n";
+            echo "\t\t<tr>\n\t\t\t<td align=\"center\"><input type=\"checkbox\" name=\"extension\" value=\"$value\"></td>\n";
+            echo "\t\t\t<td align=\"center\"><strong>$value</strong></td>\n";
         }
     }
+
+    print "</tbody>\n";
+    print "</table>\n"; // end table
+    print "<br>\n";
+    print "<p align=\"center\"><input type=\"submit\" name=\"submit\" value=\"Reload\">&nbsp;";
+    print "<input type=\"submit\" name=\"submit\" value=\"Reboot\"></p>";
+    echo "</fieldset>";
+
+} else {
+    ?>
+    <center>
+    <table border="1" cellspacing="3" cellpadding="3">
+    <thead>
+        <tr>
+            <th><center><h4>Submision Results:</h4></th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php 
+
+// Submission results
+    print "\t\t<tr>\n\t\t\t<td>Placeholder for later...</td>\n\t\t</tr>\n";
+    print "</tbody>\n";
+    echo "</table>";
+
+}
+
 ?>
+
+</center>
+
+</form>
+
+</body>
+
+</html>
