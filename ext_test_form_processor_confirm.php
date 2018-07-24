898bafa96d0d7de04d93fa772f6a131048a2aa88
Timothy Gruber <tgruber@qdusa.com>
2018-07-24 07:43:29 -0700

    Updated confirmation form processing bits

--- ext_test_form_processor_confirm.php
+++ ext_test_form_processor_confirm.php
@@ -4,13 +4,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $extension = $_POST['extension'];
 
-    if (isset($_POST['confirm'])) {
+    if (isset($_POST['confirm_reload'])) {
 
-        foreach ($extension as $extension_value) {
-            sleep(1);
+        foreach ($extension as $extension_value) { 
             echo "<center>$extension_value</center>\n";
+            //pass $extension_value to RELOAD function here.
         }
-        echo "Done.";
+        echo "Reload done.";
+    } else {
+        foreach ($extension as $extension_value) { 
+            echo "<center>$extension_value</center>\n";
+            //pass $extension_value to REBOOT function here.
+        }
+        echo "Reboot done.";
     }
 
 }
