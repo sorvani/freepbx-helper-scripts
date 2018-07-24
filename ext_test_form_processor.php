c4cb6335db09543d6ce168fc1aaae02670475818
Timothy Gruber <tjgruber@outlook.com>
2018-07-23 20:53:27 -0700

    Added html and form handling for reboot option

--- ext_test_form_processor.php
+++ ext_test_form_processor.php
@@ -39,20 +39,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     if (isset($_POST['reboot'])) {
-        echo "<p class='success'>You opted to <strong>REBOOT</strong>: $reboot</p>";
+        echo "<p class='success'>You opted to <strong>REBOOT</strong>:</p>\n";
         if (!empty($_POST["extension"])) {
             $extension = $_POST['extension'];
-            echo "<center>\n<table border=\"1\" cellspacing=\"3\" cellpadding=\"3\">\n<thead>\n<tr>\n<th><center><h4>Extension</h4></th>\n</tr>\n</thead>\n<tbody>";
+            echo "\n<table border=\"1\" cellspacing=\"3\" cellpadding=\"3\">\n<thead>\n<tr>\n<th><center><h4>Extension</h4></center></th>\n</tr>\n</thead>\n<tbody>";
             foreach ($extension as $extension_value) {
                 echo "\t\t<tr>\n\t\t\t<td><center>$extension_value</center></td>\n\t\t</tr>\n";
             }
             echo "</tbody>\n";
-            echo "</table>";
-            echo "<br><p align=\"center\"><input type=\"submit\" name=\"confirm\" value=\"Confirm REBOOT\"></p>";
+            echo "</table>\n\n";
+            ?>
+<form enctype="multipart/form-data" method="post" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>>
+            <?php 
+            echo "\n<br>\n<p align=\"center\"><input type=\"submit\" name=\"confirm\" value=\"Confirm REBOOT\"></p>";
+            echo "\n</form>";
+            echo "\n</fieldset>";
+            echo "\n</center>";
         } else {
             $extension = 0;
             $extension_value = 0;
-            echo "<p class='error'>You did not make selection!</p>";
+            echo "<p class='error'>however,</p>\n";
+            echo "<p class='error'>you did not make selection!</p>";
+            echo "\n</fieldset>";
+            echo "\n</center>";
         }
     }
 
