a509e5dc011174b23baf67aa1a7bc263fef499ec
Jared Busch <sorvani@gmail.com>
2018-07-26 16:23:08 -0500

    added commands to actually perform actions

--- ext_test_form_processor_confirm.php
+++ ext_test_form_processor_confirm.php
@@ -5,20 +5,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $extension = $_POST['extension'];
 
     if (isset($_POST['confirm_reload'])) {
-
-        foreach ($extension as $extension_value) { 
-            echo "<center>$extension_value</center>\n";
-            //pass $extension_value to RELOAD function here.
+        $astman->Login();
+        foreach ($extension as $extension_value) {
+            if ($astman->ReloadYealink($extension_value)) {
+                echo "<center>NOTIFY sent to $extension_value</center<br />\r\n";
+            } else {
+                echo "<center>NOTIFY failed to $extension_value</center<br />\r\n";
+            }
         }
+        $astman->Logout();
         echo "Reload done.";
         echo "\n</fieldset>";
         echo "\n</center>";
     } 
     if (isset($_POST['confirm_reboot'])) {
-        foreach ($extension as $extension_value) { 
-            echo "<center>$extension_value</center>\n";
-            //pass $extension_value to REBOOT function here.
+        $astman->Login();
+        foreach ($extension as $extension_value) {
+            if ($astman->RebootYealink($extension_value)) {
+                echo "<center>NOTIFY sent to $extension_value</center<br />\r\n";
+            } else {
+                echo "<center>NOTIFY failed to $extension_value</center<br />\r\n";
+            }
         }
+        $astman->Logout();
         echo "Reboot done.";
         echo "\n</fieldset>";
         echo "\n</center>";
