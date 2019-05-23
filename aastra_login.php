74ad072ce5e0bc64c54f9a502bdcfef09ab06784
Sorvani <sorvani@gmail.com>
2019-05-23 15:37:51 -0500

    added config creation functionality

--- aastra_login.php
+++ aastra_login.php
@@ -2,6 +2,7 @@
 <?php
   require_once('includes/AastraCommon.php');
   require_once('includes/AastraIPPhoneInputScreen.class.php');
+  require_once('includes/AastraIPPhoneExecute.class.php');
 
   // Get the info that was passed on the URL.
   $extension = $_GET["extension"];
@@ -27,7 +28,9 @@
         ShowBadExtension();
       } else {
         // All checks passed! Add the extension.
-        //do magic here
+        CreateEPMEntry(Aastra_decode_HTTP_header(),$extension,$desktype);
+        // Tell the phone to reset
+        ErasePhone();
       }
       break;
     case "badpass":
@@ -42,7 +45,15 @@
     default:
       ShowLoginScreen('','','');
   }
-  
+
+  function ValidateExtension($ext) {
+    // Load FreePBX bootstrap environment
+    // require_once('/etc/freepbx.conf');
+
+    return TRUE;
+  }
+
+
   function ShowLoginScreen($ext,$desk,$focus){
     $input = new AastraIPPhoneInputScreen();
     $input->setTitle('Extension Log On');
@@ -155,4 +166,48 @@
     $input->output();
   }
 
+  function ErasePhone() {
+    $execute = new AastraIPPhoneExecute();
+    $execute->addEntry('Command: Reset');
+    $execute->output();
+  }
+
+  function CreateEPMEntry($header,$ext,$desk) {
+    // Create the configuration file on the PBX.
+    // Pull information from the HTTP header to create the command.
+    $model = substr($header['model'],6);
+    $mac = strtolower($header['mac']);
+    $phoneip = $header['ip'];
+    // set template based on IP of phone.
+    // need to add this logic testing with chicago
+    $template = "chicago";
+
+    // Build the fwconsole command into a string
+    // fwconsole epm addmapping 6115-2 --brand=aastra --mac=00085d2723de --template=chicago --model=57i --account=account1
+    $fwconsole = "fwconsole endpoint addmapping ";
+    $fwconsole .= $ext."-".$desk." ";
+    $fwconsole .= "--brand=aastra ";
+    $fwconsole .= "--mac=".$mac." ";
+    $fwconsole .= "--template=".$template." ";
+    $fwconsole .= "--model=".$model." ";
+    $fwconsole .= "--account=account1";
+
+    // begin manual verification of shit during development
+    file_put_contents ("user_agent.log", "begin verification: \n", FILE_APPEND);
+    file_put_contents ("user_agent.log", "Command: ".$fwconsole." \n", FILE_APPEND);
+    file_put_contents ("user_agent.log", "end verification \n", FILE_APPEND);
+    // end manual verificatoin of shit during development
+
+    // execute the command and log the result
+    $result = shell_exec($fwconsole);
+    syslog(LOG_INFO,"Configuration file for ".str_replace("\n", "", $result));
+
+    // build the fwconsole command to to rebuild the configuration file
+    $fwconsole = "fwconsole endpoint rebuild ";
+    $fwconsole .= $ext."-".$desk;
+
+    // execute the command an log the result
+    $result = shell_exec($fwconsole);
+    syslog(LOG_INFO,"Configuration file for ".str_replace("\n", "", $result));
+  }
 ?>
