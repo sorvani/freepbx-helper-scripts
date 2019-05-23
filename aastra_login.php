4253b65f880f2c93fab837f802f5cf4497c0b6c9
Sorvani <sorvani@gmail.com>
2019-05-23 14:27:40 -0500

    pass and desk validation completed

--- aastra_login.php
+++ aastra_login.php
@@ -10,57 +10,62 @@
   $reload = $_GET["reload"];
 
   file_put_contents ("user_agent.log", "begin verification: \n", FILE_APPEND);
-  file_put_contents ("user_agent.log", "Extension: ".$extension." Password: ".$password." Desktype: ".$desktype." Bad Password: ".$badpass." Reload: ".$reload."\n", FILE_APPEND);
+  file_put_contents ("user_agent.log", "Extension: ".$extension." Password: ".$password." Desktype: ".$desktype." Password: ".$badpass." Reload: ".$reload."\n", FILE_APPEND);
   file_put_contents ("user_agent.log", "end verification \n", FILE_APPEND);
 
   // this is a reload
   switch ($reload) {
     case "submit":
-      TestFields();
+      if ($password != '5555') {
+        //the password is wrong
+        ShowBadPassword($extension,$desktype);
+      } elseif (!(($desktype == '1') OR ($desktype =='2'))) {
+        // the desktype is wrong
+        ShowBadDeskType($extension);
+      } elseif (!ValidateExtension($extension)) {
+        // the extension does not exist
+        ShowBadExtension();
+      } else {
+        // All checks passed! Add the extension.
+        //do magic here
+      }
       break;
     case "badpass":
-      ShowLoginScreen();
+      ShowLoginScreen($extension,$desktype,'badpass');
       break;
     case "baddesk":
-      ShowLoginScreen();
+      ShowLoginScreen($extension,'','baddesk');
       break;
     case "badext":
-      ShowLoginScreen();
+      ShowLoginScreen('','','');
       break;
     default:
-      ShowLoginScreen();
-  }
-
-  function TestFields() {
-    if ($password != '5555') {
-      //the password is wrong
-      ShowBadPassword($extension,$desktype);
-    } elseif (!(($desktype == '1') OR ($desktype =='2'))) {
-      // the desktype is wrong
-      ShowBadDeskType();
-    } elseif (!ValidateExtension($extension)) {
-      // the extension does not exist
-      ShowBadExtension();
-    } else {
-      // All checks passed! Add the extension.
-      //do magic here
-    }
-
+      ShowLoginScreen('','','');
   }
   
-  function ShowLoginScreen(){
+  function ShowLoginScreen($ext,$desk,$focus){
     $input = new AastraIPPhoneInputScreen();
     $input->setTitle('Extension Log On');
     $input->setDisplayMode('condensed');
     $input->setURL('http://watson.domain.com/custom/aastra/login.php?reload=submit');
     $input->setDestroyOnExit();
+    switch ($focus) {
+      case "badpass":
+        $input->setDefaultIndex(2);
+        break;
+      case "baddesk":
+        $input->setDefaultIndex(2);
+        break;
+      default:
+        $input->setDefaultIndex(1);
+    }
   
     // Ask for Extension
     $input->addField('number');
     $input->setFieldPrompt('Extension:');
     $input->setFieldParameter('extension');
-    if ($extension != '') {
-      $input->setFieldDefault("$extension");
+    if ($ext != '') {
+      $input->setFieldDefault("$ext");
     }
   
     // Ask for password
@@ -73,6 +78,9 @@
     $input->addField('number');
     $input->setFieldPrompt('Desk Type?');
     $input->setFieldParameter('desktype');
+    if ($desk != '') {
+      $input->setFieldDefault("$desk");
+    }
     // Instruction on what to enter for Main or Guest
     $input->addField('string');
     $input->setFieldPrompt('Main=1 or');
@@ -84,10 +92,6 @@
   }
 
   function ShowBadPassword($ext,$desk) {
-    file_put_contents ("user_agent.log", "begin verification: \n", FILE_APPEND);
-    file_put_contents ("user_agent.log", "http://watson.domain.com/custom/aastra/login.php?extension=".$ext."&desktype=".$desk."&reload=badpass\n", FILE_APPEND);
-    file_put_contents ("user_agent.log", "end verification \n", FILE_APPEND);
-  
     // Display bad password message and return to login script
     $input = new AastraIPPhoneInputScreen();
     $input->setTitle('Invalid password');
@@ -105,35 +109,35 @@
     $input->output();
   }
 
-  function ShowBadDeskType() {
-    // Display bad password message and return to login script
+  function ShowBadDeskType($ext) {
+    // Display bad desktype message and return to login script
     $input = new AastraIPPhoneInputScreen();
     $input->setTitle('Invalid Desk Type');
     $input->setDisplayMode('condensed');
-    $input->setURL('http://watson.domain.com/custom/aastra/login.php?extension='.$extension.'&reload=baddesk');
+    $input->setURL('http://watson.domain.com/custom/aastra/login.php?extension='.$ext.'&reload=baddesk');
     $input->setDestroyOnExit();
     $input->addSoftkey('5', 'Cancel', 'SoftKey:Exit');
     $input->addSoftkey('6', 'Retry', 'SoftKey:Submit');
     // Instruction on what to enter for Main or Guest
+    $input->addField('string');
+    $input->setFieldPrompt('Try again');
+    $input->setFieldDefault('using');
+    $input->setFieldEditable('no');
     $input->addField('empty');
     $input->addField('string');
-    $input->setFieldPrompt('User Main Desk ');
-    $input->setFieldDefault('Enter: 1');
+    $input->setFieldPrompt('Main Desk:');
+    $input->setFieldDefault('1');
     $input->setFieldEditable('no');
     $input->addField('string');
-    $input->setFieldPrompt('Visitor Desk ');
-    $input->setFieldDefault('Enter: 2');
-    $input->setFieldEditable('no');
-    $input->addField('string');
-    $input->setFieldPrompt('Please Try Again');
-    $input->setFieldDefault('');
+    $input->setFieldPrompt('Visitor Desk:');
+    $input->setFieldDefault('2');
     $input->setFieldEditable('no');
     // show it all
     $input->output();
   }
 
   function ShowBadExtension() {
-    // Display bad password message and return to login script
+    // Display bad extension message and return to login script
     $input = new AastraIPPhoneInputScreen();
     $input->setTitle('Invalid Extension');
     $input->setDisplayMode('condensed');
