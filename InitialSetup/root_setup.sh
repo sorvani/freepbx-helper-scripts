c7a6b53118604f848135a0196de9791d7da76cd2
Sorvani <sorvani@gmail.com>
2020-06-18 21:25:48 -0500

    clarifying comments and instruction

--- InitialSetup/root_setup.sh
+++ InitialSetup/root_setup.sh
@@ -2,10 +2,12 @@
 
 # Prompt for a username and password
 # TODO: change this to a parameter?
-read -p "Enter a new username: " myUserName
+read -p "Enter a new username to use for SSH access: " myUserName
 
 # Prompt for your GitLab username
 # TODO: update this to something intelligent....
+echo "For now the repo is hard coded and expecting that you have a repo named 'public_keys' containing the file `authorized_keys` in the master branch."
+echo "\n\n"
 read -p "Enter your GitLab username: " myGitLabUsername
 
 # Create user account with default password of ChangeMe
@@ -24,6 +26,7 @@ chmod 700 /home/$myUserName/.ssh
 chmod 600 /home/$myUserName/.ssh/authorized_keys
 
 # Disable root login via ssh
+echo "Disabling root loging to SSH as well as password login to SSH."
 sed -i 's/#\?\(PerminRootLogin\s*\).*$/\1 no/' /etc/ssh/sshd_config
 # Require key for ssh login
 # Ends up with duplicate PasswordAuthentication because it modifies both #PasswordAuthentication and PasswordAuthentication
