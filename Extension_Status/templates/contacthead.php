96fdb82bb0835bf85f8ef2cbb8a808dfa1d8d02c
Sorvani <sorvani@gmail.com>
2020-06-29 15:52:25 -0500

    Create contacthead.php

---
+++ Extension_Status/templates/contacthead.php
@@ -0,0 +1,37 @@
+<style>
+table {
+  width: 100%;
+}
+
+tr:hover {
+  background-color: #f5f5f5;
+}
+
+th {
+  height: 50px;
+  text-align: left;
+  background-color: #4CAF50;
+  color: black;
+}
+th, td {
+  padding: 15px;
+  border-bottom: 1px solid #ddd;
+  text-align: left;
+}
+</style>
+<div style="overflow-x:auto;">
+<table>
+  <thead>
+    <tr>
+      <th>Extension</th>
+      <th style="display:none;">URI</th>
+      <th>Brand</th>
+      <th>Model</th>
+      <th>Firmware</th>
+      <th>Status</th>
+      <th>Response Time</th>
+      <th>Device Local IP</th>
+      <th>Registration Expires</th>
+    </tr>
+  </thead>
+  <tbody>
